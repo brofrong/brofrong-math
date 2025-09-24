86fcb6f16e7c69e3a37bb43209b35cfa82979a84
dga <dima737.99@mail.ru>
2025-09-24 19:55:28 +0300

    add test

---
+++ test/math.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "bun:test";
+import { add, div, sub } from "../lib";
+
+test("add", () => {
+  expect(add(2, 2)).toBe(4);
+  expect(add(5, 8)).toBe(5 + 8);
+  expect(add(2123, 321)).toBe(2123 + 321);
+});
+
+test("div", () => {
+  expect(div(10, 2)).toBe(5);
+  expect(div(100, 10)).toBe(10);
+  expect(div(1000, 100)).toBe(10);
+});
+
+test("sub", () => {
+  expect(sub(10, 2)).toBe(8);
+  expect(sub(100, 10)).toBe(90);
+  expect(sub(1000, 100)).toBe(900);
+});
