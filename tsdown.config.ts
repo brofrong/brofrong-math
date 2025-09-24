036a104b2ef428b3ad6317594045142ed3673f37
dga <dima737.99@mail.ru>
2025-09-25 00:10:14 +0300

    move from vite  to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['./lib/index.ts'],
+  outDir: './dist',
+  format: ['cjs', 'esm', 'iife'],
+  sourcemap: true,
+  dts: true,
+  tsconfig: './tsconfig.json',
+  target: 'esnext',
+});
